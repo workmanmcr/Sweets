5fedb015b0a9551be783d9b3e9fa5c08fbc26701
workmanmcr <workmanmcr@gmail.com>
2023-08-11 11:23:27 -0700

    add db context

---
+++ Sweets/Models/SweetsContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Sweets.Models
+{
+  public class SweetsContext : IdentityDbContext<ApplicationUser>
+  {
+    public DbSet<Flavor> Flavors { get; set; }
+    public DbSet<Treat> Treats { get; set; }
+    public DbSet<TreatFlavor> TreatFlavor { get; set; }
+
+    public SweetsContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
+  }
+}
---
+++ Sweets/Models/TreatFlavor.cs
@@ -0,0 +1,11 @@
+namespace Sweets.Models
+{
+  public class TreatFlavor
+  {       
+    public int TreatFlavorId { get; set; }
+    public int TreatId { get; set; }
+    public int FlavorId { get; set; }
+    public virtual Treat Treat { get; set; }
+    public virtual Flavor Flavor { get; set; }
+  }
+}
