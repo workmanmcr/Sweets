68f43e1ab65f2d4b5095ca86646d0813e2e4938b
workmanmcr <workmanmcr@gmail.com>
2023-08-11 08:48:35 -0700

    create new mvc

---
+++ Sweets/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Sweets.Models;
+
+namespace Sweets.Controllers;
+
+public class HomeController : Controller
+{
+    
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+}
